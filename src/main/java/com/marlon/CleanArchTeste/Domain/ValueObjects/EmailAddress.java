328260c0fe5c7b844e6a769bda97dbc7123c0e41
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-07-07 23:48:12 -0300

    added value objs

--- src/main/java/com/marlon/CleanArchTeste/Domain/ValueObjects/EmailAddress.java
+++ src/main/java/com/marlon/CleanArchTeste/Domain/ValueObjects/EmailAddress.java
@@ -1,4 +1,54 @@
 package com.marlon.CleanArchTeste.Domain.ValueObjects;
 
-public class EmailAddress {
+import com.marlon.CleanArchTeste.Domain.Contracts.ValueObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailAddress implements ValueObject {
+    protected String fullAddress;
+    protected String localPart;
+    protected String domainPart;
+
+    public EmailAddress(String emailAddress) {
+        emailAddress = emailAddress.trim();
+        if (emailAddress.isEmpty()) {
+            throw new IllegalArgumentException("Endereço de e-mail não pode ser string vazia.");
+        }
+        String patternValidEmail = "^[^\\.]([\\!\\#\\$\\%\\&\\'\\*\\+\\-\\/\\=\\?\\^\\_\\`\\{\\|\\}\\~\\.\\w\\d]+)[^\\.]@([\\-\\.\\w\\d]+)$";
+        Matcher matcher = Pattern.compile(patternValidEmail).matcher(emailAddress);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Endereço de e-mail não está em formato válido.");
+        }
+        this.localPart = matcher.group(1);
+        this.domainPart = matcher.group(2);
+        this.fullAddress = emailAddress;
+    }
+
+    public String getFullAddress()
+    {
+        return this.fullAddress;
+    }
+
+    public String getLocalPart()
+    {
+        return this.localPart;
+    }
+
+    public String getDomainPart()
+    {
+        return this.domainPart;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        var map = new HashMap<String, Object>();
+        map.put("fullAddress", this.fullAddress);
+        map.put("localPart", this.localPart);
+        map.put("domainPart", this.domainPart);
+        return map;
+    }
+
 }
--- src/main/java/com/marlon/CleanArchTeste/Domain/ValueObjects/PersonName.java
+++ src/main/java/com/marlon/CleanArchTeste/Domain/ValueObjects/PersonName.java
@@ -1,4 +1,62 @@
 package com.marlon.CleanArchTeste.Domain.ValueObjects;
 
-public class PersonName {
+import com.marlon.CleanArchTeste.Domain.Contracts.ValueObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PersonName implements ValueObject {
+    protected String firstName;
+    protected String lastName;
+    protected String fullName;
+
+    public PersonName(String firstName)
+    {
+        if (firstName.isBlank()) {
+            throw new IllegalArgumentException("Primeiro nome não pode ser string vazia.");
+        }
+        this.firstName = firstName.trim();
+        this.lastName = "";
+        this.fullName = this.firstName;
+    }
+
+    public PersonName(String firstName, String lastName)
+    {
+        if (firstName.isBlank()) {
+            throw new IllegalArgumentException("Primeiro nome não pode ser string vazia.");
+        }
+        if (firstName.trim().length() > 40) {
+            throw new IllegalArgumentException("Primeiro nome não pode ter mais de 40 caracteres.");
+        }
+        if (lastName.isBlank()) {
+            throw new IllegalArgumentException("Sobrenome não pode ser string vazia.");
+        }
+        this.firstName = firstName.trim();
+        this.lastName = lastName.trim();
+        this.fullName = String.format("%s %s", this.firstName, this.lastName);
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    public String getFullName()
+    {
+        return this.fullName;
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        var map = new HashMap<String, Object>();
+        map.put("firstName", this.firstName);
+        map.put("lastName", this.lastName);
+        map.put("fullName", this.fullName);
+        return map;
+    }
 }
