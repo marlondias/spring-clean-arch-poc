b59b04410e0ff23208c0d97b8b9714e5c1037a5e
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-07-12 00:29:34 -0300

    implemented getAll of repository

--- src/main/java/com/marlon/CleanArchTeste/Adapters/Repositories/UserRepositoryImpl.java
+++ src/main/java/com/marlon/CleanArchTeste/Adapters/Repositories/UserRepositoryImpl.java
@@ -4,12 +4,22 @@ import com.marlon.CleanArchTeste.Domain.Contracts.Repositories.User.UserCommands
 import com.marlon.CleanArchTeste.Domain.Contracts.Repositories.User.UserQueriesRepository;
 import com.marlon.CleanArchTeste.Domain.Entities.User;
 import com.marlon.CleanArchTeste.Domain.ValueObjects.EmailAddress;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Component
 public class UserRepositoryImpl implements UserCommandsRepository, UserQueriesRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public UserRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public void insert(User user) {
 
@@ -36,7 +46,36 @@ public class UserRepositoryImpl implements UserCommandsRepository, UserQueriesRe
     }
 
     @Override
-    public Collection<User> getAll() {
-        return null;
+    public List<User> getAll() {
+        String sql = "SELECT * FROm users";
+        var rows = this.jdbcTemplate.queryForList(sql);
+        var users = new ArrayList<User>();
+        for (var row : rows) {
+            users.add(this.getUserFromRow(row));
+        }
+        return users;
+    }
+
+    private User getUserFromRow(Map<String, Object> userRow) {
+        userRow.values().removeIf(Objects::isNull);
+
+        var user = new User();
+        user.setId((long) userRow.get("id"));
+        user
+            .setPersonName((String) userRow.get("name"), "")
+            .setEmailAddress((String) userRow.get("email"))
+            .setHashedPassword((String) userRow.get("password"));
+
+        if (userRow.containsKey("email_verified_at")) {
+            user.setEmailVerifiedAt((String) userRow.get("email_verified_at"));
+        }
+        if (userRow.containsKey("created_at")) {
+            user.setCreatedAt((String) userRow.get("created_at"));
+        }
+        if (userRow.containsKey("updated_at")) {
+            user.setUpdatedAt((String) userRow.get("updated_at"));
+        }
+
+        return user;
     }
 }
