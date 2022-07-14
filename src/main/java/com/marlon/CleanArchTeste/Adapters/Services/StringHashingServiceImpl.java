f4bbcd3269138a1abcc11b2cba6a69767b566565
Marlon P. Dias <marlondias@users.noreply.github.com>
2022-07-14 01:57:52 -0300

    hashing service working

--- src/main/java/com/marlon/CleanArchTeste/Adapters/Services/StringHashingServiceImpl.java
+++ src/main/java/com/marlon/CleanArchTeste/Adapters/Services/StringHashingServiceImpl.java
@@ -1,19 +1,18 @@
 package com.marlon.CleanArchTeste.Adapters.Services;
 
 import com.marlon.CleanArchTeste.Domain.Contracts.Services.StringHashingService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StringHashingServiceImpl implements StringHashingService {
     @Override
     public String getPasswordHash(String password) {
-        return null;
-        //TODO
+        return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
     @Override
     public boolean checkPasswordHashMatches(String password, String hashedPassword) {
-        return false;
-        //TODO
+        return BCrypt.checkpw(password, hashedPassword);
     }
 }
